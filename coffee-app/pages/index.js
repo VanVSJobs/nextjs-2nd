217deff5200205dbf312598a335659cb7c1417bd
VanVSJobs <tranquocvan2402@gmail.com>
2021-11-09 02:44:20 -0700

    somewhat cardlayout, need to make it more userfriendly

--- coffee-app/pages/index.js
+++ coffee-app/pages/index.js
@@ -5,6 +5,8 @@ import styles from '../styles/Home.module.css';
 import Banner from '../components/banner';
 import Card from '../components/card';
 
+import coffeeStores from '../data/coffee-stores.json';
+
 export default function Home() {
   const handleOnBannerBtnClick = () => {
     console.log('hello');
@@ -24,11 +26,18 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400} />
         </div>
-        <Card
-          name='Some Coffee Store'
-          imgUrl='/static/hero-image.png'
-          href='/coffee-store/something'
-        />
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((coffeeStores) => {
+            return (
+              <Card
+                name={coffeeStores.name}
+                imgUrl={coffeeStores.imgUrl}
+                href={`/coffee-store/{coffeeStores.id}`}
+                className={styles.card}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
